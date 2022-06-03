c5ee93320fb096a56ea588707e54d28e68f2e2cb
carlos mugica <carlos@devanddel.com>
2022-06-03 09:10:55 +0200

    unnecesary code

--- src/main/java/com/dyd/pocdozer/clients/adapters/configuration/DozerMapper.java
+++ src/main/java/com/dyd/pocdozer/clients/adapters/configuration/DozerMapper.java
@@ -6,14 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayInputStream;
 import java.util.List;
 import java.util.Map;
 
 @Component
 public class DozerMapper {
 
-	private DozerBeanMapper m;
+	private DozerBeanMapper mapper;
 
 	@Autowired
 	private ApplicationContext appContext;
@@ -22,31 +21,18 @@ public class DozerMapper {
 	private List<CustomConverter> converters;
 
 	void initMapper() {
+		mapper = new DozerBeanMapper();
 
-		m = new DozerBeanMapper();
+		appContext.getBeansOfType(GenericAdapter.class).values()
+				.forEach(genericAdapter -> mapper.addMapping(genericAdapter.getBuilder()));
 
-		String varname1 = "<mappings xmlns=\"http://dozer.sourceforge.net\"" + " xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
-				+ " xsi:schemaLocation=\"http://dozer.sourceforge.net http://dozer.sourceforge.net/schema/beanmapping.xsd\" > " + "  <configuration> "
-				+ "<copy-by-references>" + "<copy-by-reference>" + " </copy-by-reference>" + "</copy-by-references>" + "  </configuration> "
-				+ "</mappings>";
-
-		Map<String, GenericAdapter> beansOfType = appContext.getBeansOfType(GenericAdapter.class);
-		for (GenericAdapter genericAdapter : beansOfType.values()) {
-			m.addMapping(genericAdapter.getBuilder());
-		}
-
-		m.setCustomConverters(converters);
-
-		m.addMapping((new ByteArrayInputStream(varname1.getBytes())));
-
-		m.map(new Object(), Object.class);
-
+		mapper.setCustomConverters(converters);
 	}
 
 	public DozerBeanMapper getMapper() {
-		if(m == null )
+		if(mapper == null )
 			initMapper();
-		return m;
+		return mapper;
 	}
 
 }
